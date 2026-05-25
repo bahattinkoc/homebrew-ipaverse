6ea937bbb4aaefda193a5cc882814b94a7a1c1d2
baaddin <61124759+bahattinkoc@users.noreply.github.com>
2026-05-25 16:58:01 +0300

    Update sha256 for ipaverse cask
    Update sha256 checksum for version 2.0.0

--- Casks/ipaverse.rb
+++ Casks/ipaverse.rb
@@ -1,6 +1,6 @@
 cask "ipaverse" do
   version "2.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "bdf6fd7e9843627a7150dabdcd2f598abbd7cd7112da0913c8cf863318e33f94"
 
   url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg"
   name "ipaverse"
