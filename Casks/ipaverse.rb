f36a31339121ceeb5b1884dbf35b3f9442433e18
baaddin <61124759+bahattinkoc@users.noreply.github.com>
2026-05-25 16:47:07 +0300

    Bump version to 2.0.0 and update description
    Updated version and SHA256 checksum for ipaverse.

--- Casks/ipaverse.rb
+++ Casks/ipaverse.rb
@@ -1,10 +1,10 @@
 cask "ipaverse" do
-  version "1.0.0"
-  sha256 "ca79f788f14acf9b5be9dac1f18981e3d6e26801f7ab3767e4f66f09974a2b87"
+  version "2.0.0"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/bahattinkoc/ipaverse/releases/download/v#{version}/ipaverse.dmg"
   name "ipaverse"
-  desc "Tool for downloading and managing iOS apps from the App Store"
+  desc "Download, resign, and sideload iOS-macOS apps from the App Store"
   homepage "https://github.com/bahattinkoc/ipaverse"
 
   depends_on macos: ">= :sonoma"
